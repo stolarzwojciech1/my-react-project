9e4f77e14e43f7b96bd2d0bb3bc84e09fda62c81
Wojciech Stolsr <stolarzwojciech1@gmail.com>
2024-12-22 11:58:08 +0100

    yours

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import {FaStar} from "react-icons/fa"
+import React, {useState} from "react"
+
+const Star=({selected=false, onSelect=f=>f}) =>(
+<FaStar color={selected? "red" : "grey"} 
+
+onClick={onSelect}  />) ;
+
+
+export default function StarRating ({totalStars=5})  {
+
+
+const [selectedStars, setSelectedStars] = useState(0);
+
+
+return( 
+<>  {createArray(totalStars).map((n, i) =>(     
+ < Star key ={i}
+     selected={selectedStars > i}
+       onSelect={() =>setSelectedStars(i+1)} 
+    /> 
+    ))}
+<p> 
+
+</p>
+</>
+);
+
+}
+
